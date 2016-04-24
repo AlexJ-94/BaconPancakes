dc6e58a60d4a616ffea237beaa51582e59148fd8
AlexJ-94 <alex.j.3456@gmail.com>
2016-04-24 14:51:24 -0500

    First Commit
    This is our first commit

---
+++ src/BaconPancakes.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class BaconPancakes {
+
+	public static void main(String[] args) 
+	{
+		JOptionPane.showMessageDialog(null, "Makin pancakes. Makin Bacon Pancakes. I take some bacon and I put it in a pancake.");
+
+	}
+
+}
